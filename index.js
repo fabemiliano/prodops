6f35d0d266da4e4ec4a18eaa03db769dfff3cf84
fabemiliano <fab.emiliano@gmail.com>
2021-07-17 21:41:15 -0300

    deploy

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser')
+const puppeteer = require('puppeteer')
+require('dotenv').config();
+
+app.use(bodyParser.json())
+
+const createPdf = async (template) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const options = {
+    path: `${__dirname}/certificate.pdf`,
+    format: 'A4'
+  }
+
+
+  await page.goto(template)
+  await page.pdf(options)
+
+}
+
+app.get('/', async (req, res, next) => {
+  res.json({ teste: 'teste' })
+})
+
+app.get('/certificate', async (req, res, next) => {
+  const { template, details: { nome, data, nomeCurso } } = req.body
+  await createPdf(template);
+  res.contentType("application/pdf");
+  res.sendFile(path.join(__dirname, '/certificate.pdf'));
+})
+
+const { PORT } = process.env
+
+app.listen(PORT, () => console.log(`escutando na porta ${PORT}`))
